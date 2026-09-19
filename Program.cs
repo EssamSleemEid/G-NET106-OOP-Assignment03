ae4872ad17668348ba7140a877e2925951e9a74a
Nongoogle <essamsleem9@gmail.com>
2026-09-19 19:43:36 +0300

    Part02 Question01

--- Program.cs
+++ Program.cs
@@ -2,6 +2,125 @@
 {
     internal class Program
     {
+        public struct DeliveryAddress
+        {
+            public string City;
+            public string Street;
+            public int BuldingNumber;
+            public DeliveryAddress(string city, string street, int buldingNumber)
+            {
+                City = city;
+                Street = street;
+                BuldingNumber = buldingNumber;
+            }
+            public string GetFullAddress()
+            {
+                return $"bulding number : {BuldingNumber}, street : {Street}, city : {City}";
+            }
+        }
+
+        public class Shipment
+        {
+            private string description;
+            private double weight;
+            private decimal deliveryFee;
+            private string trackingCode;
+            public DeliveryAddress Destination { get; set; }
+            public string TrackingCode
+            {
+                get { return trackingCode; }
+            }
+            public string Description
+            {
+                get { return description; }
+
+                set
+                {
+                    if (value != null)
+                    {
+                        description = value;
+                    }
+                }
+            }
+            public double Weight
+            {
+                get { return weight; }
+
+                set
+                {
+                    if (value > 0)
+                    {
+                        weight = value;
+                    }
+                }
+            }
+            public decimal DeliveryFee
+            {
+                get { return deliveryFee; }
+
+                set
+                {
+
+                    if (value > 0)
+                    {
+                        deliveryFee = value;
+                    }
+                }
+            }
+            public virtual decimal EstimatedCost
+            {
+                get { return DeliveryFee + (decimal)(Weight * 5); }
+            }
+            public Shipment(string trackingCode)
+            {
+                this.trackingCode = trackingCode == null ? "unknown" : trackingCode;
+
+                description = "unknown";
+                weight = 1;
+                deliveryFee = 50;
+                Destination = new DeliveryAddress("Cairo", "Unknown Street", 0);
+            }
+            public Shipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
+            {
+                this.trackingCode = trackingCode == null ? "Unknown" : trackingCode;
+                this.description = description == null ? "Unknown" : description;
+                this.weight = weight > 0 ? weight : 1;
+                this.deliveryFee = deliveryFee > 0 ? deliveryFee : 50;
+                Destination = destination;
+            }
+            public void UpdateDeliveryFee(decimal newFee)
+            {
+                if (newFee > 0)
+                {
+                    deliveryFee = newFee;
+                }
+            }
+            public void UpdateWeight(double newWeight)
+            {
+                if (newWeight > 0)
+                {
+                    Weight = newWeight;
+                }
+            }
+            public void UpdateWeight(double newWeight, double packingWeight)
+            {
+                if (newWeight > 0 && packingWeight >= 0)
+                {
+                    Weight = newWeight + packingWeight;
+                }
+            }
+            public virtual void PrintShipment()
+            {
+                Console.WriteLine("trackingCode : " + TrackingCode);
+                Console.WriteLine("description : " + Description);
+                Console.WriteLine("weight : " + Weight + " kg");
+                Console.WriteLine("deliveryFee : " + DeliveryFee);
+                Console.WriteLine("Destination : " + Destination.GetFullAddress());
+                Console.WriteLine("estimatedCost : " + EstimatedCost + " egy");
+            }
+        }
+
+
         static void Main(string[] args)
         {
             #region Part01
@@ -37,6 +156,11 @@
 
             //no bc sealed prevent further ovveriding of that method 
             #endregion
+            #endregion
+
+            #region Part02
+
+
             #endregion
         }
     }
